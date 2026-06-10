05239f00411d9ef98e026b4cf0a73699a427c38e
Flipmixplay <mironhmel@gmail.com>
2026-06-11 01:20:24 +0500

    "Fix: make image path relative for GitHub Pages

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+//thx for code to safey(safey.neocities.org)
+function getYearsSince(dateString) {
+  const today = new Date();
+  const pastDate = new Date(dateString);
+
+  let years = today.getFullYear() - pastDate.getFullYear();
+
+  const monthDiff = today.getMonth() - pastDate.getMonth();
+  const dayDiff = today.getDate() - pastDate.getDate();
+
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    years--;
+    }
+      return years;
+    }
+    // not my real birthday
+    document.getElementById('age').textContent = getYearsSince("2006-10-01");
+
 async function loadDotaStats() {
     try {
-        // Читаем локальный JSON-файл
         const response = await fetch('stats.json');
         const data = await response.json();
 
-        // Вставляем данные в HTML
         document.getElementById('dota-rank').textContent = data.rank || 'Unranked';
         document.getElementById('dota-winrate').textContent = data.winrate || '0%';
         document.getElementById('dota-hero').textContent = data.top_hero || 'Unknown';
